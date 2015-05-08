c74fa3fa329846e4f15889cdac651df8f2cb64b6
James Gregory <james.jrg@gmail.com>
2015-05-08 21:20:17 +0100

    a couple more grid tests

--- Battleships.Tests/GridTests.cs
+++ Battleships.Tests/GridTests.cs
@@ -22,9 +22,27 @@ namespace Battleships.Tests
         }
 
         [TestMethod]
-        public void ShouldGetLineOfSquares()
+        public void ShouldGetVerticalLineOfSquares()
         {
-            //skipped this for now
+            var grid = new Grid(10);
+
+            grid.Squares[0, 2] = new Square {Contents = SquareContents.Ship};
+            var verticalLine = grid.GetLineOfSquares(new Coordinates(0, 0), true, 3);
+            CollectionAssert.AreEqual(
+                new[] {SquareContents.Sea, SquareContents.Sea, SquareContents.Ship},
+                verticalLine.Select(x => x.Contents).ToArray());
+        }
+
+        [TestMethod]
+        public void ShouldGetHorizontalLineOfSquares()
+        {
+            var grid = new Grid(10);
+
+            grid.Squares[2, 0] = new Square { Contents = SquareContents.Ship };
+            var horizontalLine = grid.GetLineOfSquares(new Coordinates(0, 0), false, 3);
+            CollectionAssert.AreEqual(
+                new[] { SquareContents.Sea, SquareContents.Sea, SquareContents.Ship },
+                horizontalLine.Select(x => x.Contents).ToArray());
         }
     }
 }
